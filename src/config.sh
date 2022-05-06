bdde495c11d8805ffae9c518a639eded82219b5c
andrewzhao <55998423+yaozcoderepo@users.noreply.github.com>
2022-05-06 15:22:19 -0400

    Update config.sh

--- src/config.sh
+++ src/config.sh
@@ -7,5 +7,5 @@
                 min_on=5 max_on=5 on_incr=0 \
                 min_off=0.2 max_off=0.2 off_incr=0 \
                 min_sloss=0.02 max_sloss=0.02 sloss_incr=0 \
-                ticks=3000 \
+                ticks=5000 \
                 min_buf_size=1000 max_buf_size=3000 buf_size_incr=1000
