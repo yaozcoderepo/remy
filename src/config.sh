3b5e79ed5d619ee2fcbf9808c933685a9d8fc02a
andrewzhao <55998423+yaozcoderepo@users.noreply.github.com>
2022-05-06 00:51:46 -0400

    Update config.sh

--- src/config.sh
+++ src/config.sh
@@ -2,7 +2,7 @@
 
 ./configuration of=testing \
                 min_link_ppt=15 max_link_ppt=15 link_ppt_incr=0 \
-                min_rtt=75 max_rtt=75 rtt_incr=0 \
+                min_rtt=150 max_rtt=150 rtt_incr=0 \
                 min_nsrc=8 max_nsrc=8 nsrc_incr=0 \
                 min_on=5 max_on=5 on_incr=0 \
                 min_off=0.2 max_off=0.2 off_incr=0 \
