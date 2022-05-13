ac7ec57f004fcc0f094e1e9850c08051f4ba30c7
andrewzhao <yaoz@gmail.com>
2022-05-13 19:24:25 -0400

    change readme

--- src/config.sh
+++ src/config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ./configuration of=testing \
-                min_link_ppt=10 max_link_ppt=20 link_ppt_incr=2 \
-                min_rtt=100 max_rtt=200 rtt_incr=20 \
-                min_nsrc=1 max_nsrc=16 nsrc_incr=2 \
-                min_on=5 max_on=5 on_incr=0 \
-                min_off=5 max_off=5 off_incr=0 \
-                min_sloss=0.01 max_sloss=0.03 sloss_incr=0.01 \
-                ticks=500 \
-                min_buf_size=100000 max_buf_size=100000 buf_size_incr=0
+                min_link_ppt=1 max_link_ppt=1 link_ppt_incr=0 \
+                min_rtt=150 max_rtt=150 rtt_incr=0 \
+                min_nsrc=8 max_nsrc=8 nsrc_incr=0 \
+                min_on=5000 max_on=5000 on_incr=0 \
+                min_off=5000 max_off=5000 off_incr=0 \
+                min_sloss=0 max_sloss=0 sloss_incr=0 \
+                ticks=200000 \
+                min_buf_size=inf max_buf_size=inf buf_size_incr=0
